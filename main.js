9927b220e2d5e02702b6f012a270f8741dbf44ee
Lucas Barreiro <Lucasytube17@gmail.com>
2023-09-25 16:00:26 -0300

    upload
    Andrés Cardoso
    Gastón Techera
    Martin Cabrera
    Lucas Barreiro

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const feed=document.querySelector(".tweets");
+const feed=document.querySelector(".feed");
 const API_URL="https://my-json-server.typicode.com/MrGalletah/fake-twitter-API/posts";
 console.log(feed)
 
@@ -12,5 +12,22 @@ fetch(API_URL)
 function agregarTweets(array){
     feed.innerHTML="";
     console.log(array);
-
+    for (const tweet of array) {
+        feed.innerHTML+=` <div class="profile-pic"><img src="https://picsum.photos/10" /></div>
+        <div class="content">
+          <div class="names">
+            <p class="full-name">${tweet.name}</p>
+            <p class="user-name">${tweet.name.replace(" ","_").toLowerCase()}</p>
+            <p class="time">${new Date().toLocaleString()}</p>
+          </div>
+        </div>
+        <div class="tweet-content">
+          <p>${tweet.text}</p>
+        </div>
+        <div class="tweet-icons">
+          <i class="fa fa-comment" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+        </div>`
+    }
 }
